fb00d9ea5981e36a1d72c762435964f9c6c772a1
remycharles <remy.charles@algosup.com>
2023-11-17 14:38:53 +0100

    Update each asm file
    add commentary on each asm file for the engineer

---
+++ PacManProject/src/memory/memory.asm
@@ -0,0 +1,10 @@
+;----------------------------------------------------------
+;
+; Focuses on efficient memory management within the game,
+; handling dynamic allocation, memory pooling, and garbage 
+; collection to optimize performance.
+;
+; Maximizes the game's performance by efficient use of 
+; limited memory resources.
+;
+;----------------------------------------------------------
---
+++ PacManProject/src/rendering/ui.asm
@@ -0,0 +1,10 @@
+;---------------------------------------------
+;
+; Dedicated to rendering the user interface 
+; elements like the score display, lives, 
+; and game menus.
+;
+; Enhances user experience through clear and 
+; responsive UI elements.
+;
+;---------------------------------------------
