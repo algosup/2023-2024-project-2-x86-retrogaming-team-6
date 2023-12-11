e23831f7e9cab560274aff432c9b645f82273bf7
remycharles <remy.charles@algosup.com>
2023-12-11 13:46:46 +0100

    update maze

--- Prototype/launch-dosbox.sh
+++ Prototype/launch-dosbox.sh
@@ -6,9 +6,14 @@ thisDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rootDir="$(cd "$thisDir" && pwd)"
 binDir="$rootDir/bin"
 
+cd "${thisDir}/src/" \
+  && nasm main.asm -f bin -o "${binDir}/main.com" \
+
+echo "Built Successfully! ✅"
+
 ROOT_DIR=$(dirname "$0")
 BIN_DIR="$binDir"
 DOSBOX_BIN="/opt/homebrew/bin/dosbox"
 CONFIG_LOC="$ROOT_DIR"
 
-"$DOSBOX_BIN" -c "MOUNT c $BIN_DIR" -c "C:" -c "keyb fr"
+"$DOSBOX_BIN" -c "MOUNT c $BIN_DIR" -c "C:" -c "keyb fr" -c "main.com"
